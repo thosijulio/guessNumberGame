12e06bbb29054d389f95ca562e8de69ab93a7182
thosijulio <thosijulio@gmail.com>
2022-12-25 18:29:43 -0300

    utilizando cout para imprimir dados no terminal

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,5 +1,13 @@
-#include<stdio.h>
+/*
+    Padrao para adição de bibliotecas em C++: 
+        "#include" e o nome da biblioteca que será utilizada entre "<>"
+*/
+// iostream é uma biblioteca que possui métodos para o fluxo de entra e saída de dados.
+#include<iostream>
 
+// main: Função que guarda todo o código que será executado
+// pelo compilador quando abrir este arquivo.
 int main(void){
-    printf("Hello world");
+    // cout é a função padrão do C++ que lida com as saídas de um programa pelo terminal.
+    std::cout << "Hello world";
 }
