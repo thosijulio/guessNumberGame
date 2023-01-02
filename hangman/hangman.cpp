2386651521793a1d25df287d054e3e1a96e4bb11
thosijulio <thosijulio@gmail.com>
2023-01-02 11:33:49 -0300

    FEAT - criando funcao que verifica se letra digitada existe na palavra armazenada

--- hangman/hangman.cpp
+++ hangman/hangman.cpp
@@ -5,13 +5,27 @@ const std::string SECRET_WORD = "teste de string";
 bool userWin = false;
 bool gameOver = false;
 
+bool letterExist(char letter) {
+  for (int index = 0; index < SECRET_WORD.size(); ++ index) {
+    if (letter == SECRET_WORD[index]) {
+      return true;
+    } else return false;
+  }
+}
+
 int main() {
 
-  char word;
+  char letter;
 
   while (!userWin && !gameOver) {
     std::cout << "Digite uma letra: ";
-    std::cin >> word;
-    std::cout << word << "\n";
+    std::cin >> letter;
+
+    if (letterExist(letter)) {
+      std::cout << "Você acertou! Continue\n";
+    } else {
+      std::cout << "Você errou! Tente novamente\n";
+    }
+    std::cout << letter << "\n";
   }
 }
